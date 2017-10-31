9729a2ac71430b277340031ad4270fb153d6cae8
Ben Goldhaber <goldhaber.ben@gmail.com>
2017-10-31 14:56:43 -0700

    modified visualization

--- daemon_cli.sh
+++ daemon_cli.sh
@@ -6,8 +6,10 @@ case "$1" in
   ./build/node 3000 127.0.0.1 2006 1 &
   ./build/node 3001 127.0.0.1 3000 2 &
   ./build/node 3002 127.0.0.1 3001 4 &
+  sleep 5
   ./build/node 3003 127.0.0.1 3002 7 &
   ./build/node 3004 127.0.0.1 3003 20 &
+  sleep 5
   ./build/node 3005 127.0.0.1 3004 22 &
   ./build/node 3006 127.0.0.1 3005 40 &
 
@@ -21,6 +23,7 @@ case "$1" in
   echo "45:NAN:NAN:GET_STATS:REQ:" > /dev/udp/127.0.0.1/3005
   echo "45:NAN:NAN:GET_STATS:REQ:" > /dev/udp/127.0.0.1/3006
   ;;
+
   put)
   echo "2:127.0.0.1:3001:QUERY_CHORD_PUT:REQ:$2" > /dev/udp/127.0.0.1/3001
   ;;
